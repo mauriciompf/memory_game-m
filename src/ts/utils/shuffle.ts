cfc2d7ba71fa0b09f406cdeafc67d06232223f97
mpfarias <fariasmauricio11@outlook.com>
2024-10-05 12:24:38 -0300

    feat: add shuffle

---
+++ src/ts/utils/shuffle.ts
@@ -0,0 +1,16 @@
+const shuffle = <T>(array: T[]): T[] => {
+  let currentIndex = array.length;
+
+  while (currentIndex !== 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+};
+
+export default shuffle;
