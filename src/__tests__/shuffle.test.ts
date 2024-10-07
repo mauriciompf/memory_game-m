5206ce4f27ce452967ffd8bc5c70ebd9848f4c38
mpfarias <fariasmauricio11@outlook.com>
2024-10-07 11:02:01 -0300

    test: shuffle

---
+++ src/__tests__/shuffle.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from "vitest";
+import shuffle from "../ts/utils/shuffle";
+
+const originalArray = [1, 2, 3, 4, 5];
+const shuffledArray = shuffle([...originalArray]);
+
+describe("shuffle", () => {
+  it("should contain the same elements as the original", () => {
+    expect(shuffledArray).toHaveLength(originalArray.length);
+  });
+
+  it("should shuffle an array of numbers and change the order", () => {
+    expect(shuffledArray).not.toEqual(originalArray);
+  });
+
+  it("should not lose or duplicate elements during shuffle", () => {
+    expect(shuffledArray.sort()).toEqual(originalArray.sort());
+  });
+});
