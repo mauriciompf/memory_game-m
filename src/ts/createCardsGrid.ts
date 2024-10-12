6f7f75867c60fc932ab54badd769aea4c26d1ace
mpfarias <fariasmauricio11@outlook.com>
2024-10-12 19:26:26 -0300

    refactor: createCardsGrid

--- src/ts/createCardsGrid.ts
+++ src/ts/createCardsGrid.ts
@@ -2,9 +2,10 @@ import fetchEmojiData from "./utils/fetchEmojiData";
 import shuffle from "./utils/shuffle";
 
 const createCardsGrid = async () => {
-  let cardButtonElements = ""; // To hold all the button elements
   const cardsGrid = document.getElementById("cards-grid") as HTMLDivElement;
   if (!cardsGrid) return;
+
+  cardsGrid.innerHTML = "";
 
   let shuffledEmojiCards = JSON.parse(
     localStorage.getItem("shuffledEmojiCards") || "null"
@@ -16,7 +17,7 @@ const createCardsGrid = async () => {
 
     // Create array of emojis
     const singleEmojis = [];
-    const usedIndices = new Set<number>(); // To track <unique> used indices
+    const usedIndices = new Set<number>(); // Track <unique> used indices
 
     for (let i = 0; i < 5; i++) {
       let randomIndex;
@@ -27,14 +28,14 @@ const createCardsGrid = async () => {
         randomIndex = Math.floor(Math.random() * emojiData.length);
       } while (usedIndices.has(randomIndex));
 
-      usedIndices.add(randomIndex); // Preventing use again
+      usedIndices.add(randomIndex); // Prevent use again
 
       const emojiCharacter = emojiData[randomIndex].char; // Get a random emoji
       singleEmojis.push(emojiCharacter);
     }
 
-    // Duplicate every single emoji and shuffle
-    const fullEmojiCards = singleEmojis.flatMap((e) => [e, e]);
+    const fullEmojiCards = singleEmojis.flatMap((e) => [e, e]); // Duplicate every single emoji
+
     shuffledEmojiCards = shuffle(fullEmojiCards);
 
     localStorage.setItem(
@@ -44,29 +45,25 @@ const createCardsGrid = async () => {
   }
 
   for (let i = 0; i < shuffledEmojiCards.length; i++) {
-    // Concatenating each button element to the string
-    cardButtonElements += `
-        <button
-            id="cardId-${i}"
-            class="relative shadow-xl transition-all duration-500 [transform-style:preserve-3d] select-none place-items-center grid rounded-2xl bg-white font-bold text-3xl size-16 "
-        >
-            <div
-              id="front-card-${i}"
-              class="absolute inset-0 grid place-items-center [backface-visibility:hidden] [transform:rotateY(180deg)]"
-            >
-              ${shuffledEmojiCards[i]}
-            </div>
-        
-            <div
-              id="back-card-${i}"
-              class="absolute inset-0 grid place-items-center [backface-visibility:hidden]"
-            >
-              ?
-            </div>
-        </button>`;
-  }
+    const button = document.createElement("button");
+    button.ariaLabel = "Emoji card with back face showing";
+    button.className =
+      "relative shadow-xl transition-all duration-500 [transform-style:preserve-3d] select-none place-items-center grid rounded-2xl bg-white font-bold text-3xl size-16";
+    button.id = `cardId-${i}`;
 
-  cardsGrid.innerHTML = cardButtonElements;
+    const frontCard = document.createElement("div");
+    frontCard.className =
+      "absolute inset-0 grid place-items-center [backface-visibility:hidden] [transform:rotateY(180deg)]";
+    frontCard.textContent = shuffledEmojiCards[i];
+
+    const backCard = document.createElement("div");
+    backCard.className =
+      "absolute inset-0 grid place-items-center [backface-visibility:hidden]";
+    backCard.textContent = "?";
+
+    button.append(frontCard, backCard);
+    cardsGrid.appendChild(button);
+  }
 
   return shuffledEmojiCards;
 };
