b4410f2513ba948d35c03d52a9be46d0fdfcc70b
mpfarias <fariasmauricio11@outlook.com>
2024-10-12 20:16:22 -0300

    feat: increment n tests

--- src/__tests__/flipCards.test.ts
+++ src/__tests__/flipCards.test.ts
@@ -1,33 +1,58 @@
 import { describe, it, expect, beforeEach } from "vitest";
 import { fireEvent } from "@testing-library/dom";
 import flipCards from "../ts/flipCards";
+import disableAllCardButtons from "../ts/utils/disableAllCardButtons";
+import enableAllCardButtons from "../ts/utils/enableAllCardButtons";
+import saveMatchedCards from "../ts/utils/saveMatchedCards";
 
-describe("flipCards", () => {
-  for (let i = 0; i < 10; i++) {
-    beforeEach(() => {
-      document.body.innerHTML = "";
+describe("flipCards", async () => {
+  beforeEach(() => {
+    document.body.innerHTML = '<div id="cards-grid"></div>'; // Parent element
+  });
 
-      for (let i = 0; i < 10; i++) {
-        const button = document.createElement("button");
-        button.id = `cardId-${i}`;
-        document.body.appendChild(button);
-      }
-    });
-  }
+  await flipCards();
 
-  it("should add in cardId-0 the class [transform:rotateY(180deg)] when clicked", async () => {
-    await flipCards();
+  const firstCard = document.getElementById("cardId-0") as HTMLDivElement;
+  const secondCard = document.getElementById("cardId-1") as HTMLDivElement;
 
-    const cardButton = window.document.getElementById(
-      "cardId-0"
-    ) as HTMLButtonElement;
+  it("should add the class [transform:rotateY(180deg)] to cardId-0 when clicked", () => {
+    if (!firstCard) return;
 
-    if (!cardButton) return;
+    fireEvent.click(firstCard);
 
-    fireEvent.click(cardButton);
-
-    expect(cardButton.classList.contains("[transform:rotateY(180deg)]")).toBe(
+    expect(firstCard.classList.contains("[transform:rotateY(180deg)]")).toBe(
       true
     );
+  });
+
+  it("should disable and enable card buttons after two clicks", () => {
+    if (!firstCard && !secondCard) return;
+
+    fireEvent.click(firstCard);
+    fireEvent.click(secondCard);
+
+    expect(disableAllCardButtons).toHaveBeenCalledOnce();
+    expect(enableAllCardButtons).toHaveBeenCalledOnce();
+  });
+
+  it("should save matched cards when two matching cards are flipped ", () => {
+    if (!firstCard && !secondCard) return;
+
+    fireEvent.click(firstCard);
+    fireEvent.click(secondCard);
+
+    expect(saveMatchedCards).toHaveBeenCalled();
+  });
+
+  it("should flip unmatched cards back after a delay", () => {
+    if (!firstCard && !secondCard) return;
+
+    fireEvent.click(firstCard);
+    fireEvent.click(secondCard);
+
+    setTimeout(() => {
+      expect(firstCard.classList).not.toContain("[transform:rotateY(180deg)]");
+      expect(secondCard.classList).not.toContain("[transform:rotateY(180deg)]");
+    }, 1000);
   });
 });
