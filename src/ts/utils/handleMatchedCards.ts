cbef563c4e31e5d4f1cd50322589c0c9f2bbc393
mpfarias <fariasmauricio11@outlook.com>
2024-10-23 09:48:13 -0300

    feat: add win sound

--- src/ts/utils/handleMatchedCards.ts
+++ src/ts/utils/handleMatchedCards.ts
@@ -1,21 +1,30 @@
-import { cardButtons, cardsMatched, flippedEmojis } from "../flipCards";
+import { cardButtons, flippedEmojis } from "../flipCards";
 import winGame from "../winGame";
+import cardsMatched from "./cardMatched";
 import resetArrays from "./resetArrays";
 import saveMatchedCards from "./saveMatchedCards";
 import stopTimer from "./stopTimer";
+import audio from "../../meme.mp3";
 
 const handleMatchedCards = (cardButton: HTMLButtonElement) => {
-  const previousCard = cardButtons[0];
-  cardsMatched.add(previousCard);
-  cardsMatched.add(cardButton);
-  previousCard.disabled = true;
-  cardButton.disabled = true;
+  const yupii = new Audio(audio);
+  yupii.volume = 0.2;
 
-  saveMatchedCards(Array.from(cardsMatched));
+  const previousCard = cardButtons[0];
+
+  cardsMatched.addAtributes(previousCard, cardButton);
+
+  saveMatchedCards(Array.from(cardsMatched)); // Save in localStorage
+
   resetArrays(flippedEmojis, cardButtons); // Clear arrays for the next flip attempt
 
+  // Win condition
   if (cardsMatched.size === 12) {
+    if (parseInt(localStorage.getItem("seconds") || "") <= 30)
+      yupii.play().catch((error) => console.error("Audio failed:", error));
+
     stopTimer();
+
     setTimeout(winGame, 1500);
   }
 };
